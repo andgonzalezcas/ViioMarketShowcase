16351beebb7c89c5b21c1e3d724908b432a6899d
Andres <andgonzalezcas@unal.edu.co>
2023-11-16 16:43:16 -0500

    Se creo el proyecto frontend con la configuracion inicial

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './style/global.css'
+
+function App() {
+  return (
+    <div className='bg-[#131313] w-full h-screen text-white'>
+      <p className='text-7xl'>VIIO</p>
+    </div>
+  );
+}
+
+export default App;
