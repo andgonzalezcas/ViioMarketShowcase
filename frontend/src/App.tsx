ab44b0cc7de7f094de06b92bca0483cbb5ef84fb
Andres <andgonzalezcas@unal.edu.co>
2023-11-17 07:42:18 -0500

    change react app to vite react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import './style/global.css'
-
 function App() {
   return (
-    <div className='bg-[#131313] w-full h-screen text-white'>
-      <p className='text-7xl'>VIIO</p>
-    </div>
-  );
+    <div>Holiwis</div>
+  )
 }
 
-export default App;
+export default App
