6cf66d5383728c0b99f3cd98469ff3ad25776eae
Andres <andgonzalezcas@unal.edu.co>
2023-11-18 08:12:16 -0500

    update style on frontend gived new button on navigation store

--- frontend/src/components/storeNavbar.tsx
+++ frontend/src/components/storeNavbar.tsx
@@ -13,7 +13,8 @@ const StoreNavbar = () => {
     <nav className="flex flex-col md:flex-row justify-between items-start md:items-center">
       <Link to={'/store/'} className="text-xl"><b>Store</b></Link>
 
-      <div className="w-full md:w-fit flex justify-between gap-2">
+      <div className="w-full md:w-fit flex flex-wrap justify-between gap-2">
+        <Link to={'/store/categories'} className="p-1 border rounded-lg">Search</Link>
         <Link to={'/store/categories'} className="p-1 border rounded-lg">Categories</Link>
         <Link to={'/store/cart'} className="p-1 border rounded-lg">Cart</Link>
         <button onClick={() => handleLogout()} className="p-1 border rounded-lg">Log out</button>
