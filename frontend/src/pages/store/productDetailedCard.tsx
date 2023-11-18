37070454f2c0fa10aaad3adc322a4334831d3aaf
Andres <andgonzalezcas@unal.edu.co>
2023-11-18 06:06:20 -0500

    Se agrego el producto detallado por id

--- frontend/src/pages/store/productDetailedCard.tsx
+++ frontend/src/pages/store/productDetailedCard.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { useParams } from "react-router-dom"
 import { useAppSelector } from "../../redux/hooks";
+import { AiFillStar } from "react-icons/ai";
 
 const ProductDetailedCard = () => {
   const params = useParams();
@@ -18,7 +19,34 @@ const ProductDetailedCard = () => {
   }, [params.productId, productsList?.length])
 
   return (
-    <p>{product.title}</p>
+    <div className="w-full h-[80vh] flex justify-center items-center">
+      <div className="max-w-xl w-full border p-8 rounded-2xl flex flex-col gap-5">
+        <div className="h-80 overflow-hidden rounded-lg">
+          <img
+            src={product.thumbnail}
+            alt="product card image"
+            className="w-full min-w-[166px] h-full object-cover"
+          />
+        </div>
+        <p><b>{product.title}</b></p>
+        <p>{product.description}</p>
+        <div className="w-full flex flex-col md:flex-row justify-between">
+          <p>Price: {product.price}</p>
+          <p className="text-yellow-300">-{product.discountPercentage}% DCTO</p>
+          <p>On Stock: {product.stock}</p>
+          <div className="flex items-center gap-1">
+            <AiFillStar className="text-yellow-400" />
+            <p>{product.rating}</p>
+          </div>
+        </div>
+        <div className="w-full flex flex-col md:flex-row justify-between">
+          <p>Brand: {product.brand}</p>
+          <p>Category: {product.category}</p>
+        </div>
+        <button className="p-2 w-full text-center border rounded-lg" onClick={() => alert('this functionality comming soon!')}>Add to cart</button>
+      </div>
+
+    </div>
   )
 }
 
